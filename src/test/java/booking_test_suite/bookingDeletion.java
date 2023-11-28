48b1e59c8d09af9710ed41bcff7436d43b4df957
marnitz.delange <marnitz411@icloud.com>
2023-11-28 16:48:31 +0200

    Refactored deletion test

--- src/test/java/booking_test_suite/bookingDeletion.java
+++ src/test/java/booking_test_suite/bookingDeletion.java
@@ -1,9 +1,14 @@
 package booking_test_suite;
 
+import java.time.Duration;
+import java.util.Arrays;
+import java.util.List;
 import java.util.regex.Pattern;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.*;
 import static org.testng.Assert.*;
 import org.openqa.selenium.*;
@@ -32,12 +37,12 @@ public class bookingDeletion {
     }
 
     @Test
-    public void testUntitledTestCase() throws Exception {
+    public void deleteCreatedBooking() throws Exception {
 
         driver.get("https://automationintesting.online/");
         driver.findElement(By.xpath("//div[@id='root']/div[2]/div/div[4]/div/div/div[3]/button")).click();
-        WebElement sourceDate = driver.findElement(By.xpath("/html/body/div/div/div/div[4]/div/div[2]/div[2]/div/div[2]/div[4]/div[2]/div[1]/div[2]/button"));
-        WebElement targetDate = driver.findElement(By.xpath("/html/body/div/div/div/div[4]/div/div[2]/div[2]/div/div[2]/div[4]/div[2]/div[1]/div[5]/button"));
+        WebElement sourceDate = driver.findElement(By.xpath("//button[text()='14']"));
+        WebElement targetDate = driver.findElement(By.xpath("//button[text()='17']"));
         dragAndDrop(sourceDate, targetDate);
         driver.findElement(By.name("firstname")).click();
         driver.findElement(By.name("firstname")).clear();
@@ -49,8 +54,13 @@ public class bookingDeletion {
         driver.findElement(By.name("phone")).clear();
         driver.findElement(By.name("phone")).sendKeys("987654321112");
         driver.findElement(By.xpath("//div[@id='root']/div/div/div[4]/div/div[2]/div[3]/button[2]")).click();
-        driver.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Automation in Testing'])[1]/following::div[7]")).click();
-        driver.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Booking Successful!'])[1]/following::button[1]")).click();
+
+
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        WebElement modal = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[@tabindex='-1']")));
+
+        driver.findElement(By.xpath("//button[text()='Close']")).click();
+
         driver.findElement(By.linkText("Admin panel")).click();
         driver.findElement(By.id("username")).click();
         driver.findElement(By.id("username")).clear();
@@ -58,18 +68,39 @@ public class bookingDeletion {
         driver.findElement(By.id("password")).clear();
         driver.findElement(By.id("password")).sendKeys("password");
         driver.findElement(By.id("doLogin")).click();
+
         driver.findElement(By.xpath("//div[@id='room1']/div[2]")).click();
 
-        //Admin booking
-        //booking Name 1: /html/body/div/div/div/div/div/div/div[3]/div[1]/div/div/div[1]
-        //booking Name 2: /html/body/div/div/div/div/div/div/div[3]/div[2]/div/div/div[1]
-        //booking Name 3: /html/body/div/div/div/div/div/div/div[3]/div[3]/div/div/div[1]/p
-        //Here I need code to loop through the names and and when it finds the booking created it must delete it,
-        // I also need an assertion to confirm that the booking has been deleted
+        // XPath for the container of booking details
+        String bookingContainerXPath = "//div[@class='detail booking-1']";
+
+        // Get all booking containers
+        List<WebElement> bookingContainers = driver.findElements(By.xpath(bookingContainerXPath));
+
+        boolean bookingFound = false;
+
+        // Loop through booking containers
+        for (WebElement bookingContainer : bookingContainers) {
+            // Extract first name from the booking container
+            String firstName = bookingContainer.findElement(By.cssSelector(".col-sm-2 > p:nth-of-type(1)")).getText();
+
+            System.out.println("Found Booking: " + firstName);
+
+            // Check if the first name matches the one you want to delete
+            if (firstName.equals("Booking")) {
+                // Found the booking, perform deletion
+                System.out.println("Deleting Booking: " + firstName);
+                bookingContainer.findElement(By.cssSelector(".col-sm-1 > span.fa-trash")).click();
+                // Add additional steps if needed (e.g., confirm deletion)
+                bookingFound = true;
+                break; // Break the loop after deleting the booking
+            }
+        }
+
+        // Assert that the booking was found and deleted
+        assertTrue(bookingFound, "Booking was not found for deletion");
 
 
-        driver.findElement(By.xpath("//div[@id='root']/div/div/div/div/div/div[3]/div[3]/div/div/div")).click();
-        driver.findElement(By.xpath("//div[@id='root']/div/div/div/div/div/div[3]/div[3]/div/div/div[7]/span[2]")).click();
     }
 
     @AfterClass(alwaysRun = true)
